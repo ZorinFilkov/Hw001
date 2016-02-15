8f6f56412408a994a9c1a835313409962c21e9aa
ZorinFilkov <zfilkov@abv.bg>
2016-02-15 21:04:35 +0200

    My first Home Work

---
+++ src/Homework.java
@@ -0,0 +1,15 @@
+
+public class Homework {
+
+	public static void main(String[] args) {
+		System.out.println("Hallo loser");
+		System.out.println("Hi ");
+		System.out.println("How are you");
+		System.out.println("Fine,I'm doing my homework");
+		System.out.println("It's too easy");
+		System.out.println("Ok");
+		System.out.println("Stay at School");
+
+	}
+
+}
